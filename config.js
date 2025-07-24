a8f3dfb7c1ad6621902743a9acb98d1726993137
lodubot <jaihacku@gmail.com>
2025-07-24 13:17:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
         return (
             {
                 adminUsers:[7871019658], //admin users' telegram id, should be numbers
-                botToken: '7574903711:AAHHTcBwV81snxaqDUddKe-qH5tg_lynfOU', // bot token
+                botToken: '7649982058:AAGETXQt_DZPKEDIePcCHP_Q5VlS7kfQKQ4', // bot token
 
             }
         );
